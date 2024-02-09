3e884d7375d98221033aa5660d1472bc9d7c772f
CHROG <rogowsky@chrog.net>
2024-02-09 11:38:33 +0100

    Update logon.sh

--- logon.sh
+++ logon.sh
@@ -8,4 +8,4 @@ echo "mount_smbfs //10.1.1.2/pgm$ /Users/\$mluser/Volumes/Programme/" >> /Users/
 echo "mount_smbfs //10.1.1.2/mldata/benutzer/lul/\$mluser /Users/\$mluser/Volumes/Home" >> /Users/logon.command
 echo "mount_smbfs //10.1.1.2/mldata/benutzer/sus/\$mluser /Users/\$mluser/Volumes/Home" >> /Users/logon.command
 chmod 755 /Users/logon.command
-#if [ -d /Users/$3/Desktop/Laufwerke ]; then ln -sf /Users/logon.command /Users/$3/Desktop/Laufwerke; fi
+#if [ ! -f /Users/$3/Desktop/Laufwerke ]; then ln -sf /Users/logon.command /Users/$3/Desktop/Laufwerke; fi
