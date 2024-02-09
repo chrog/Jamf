3ba14c02af4b921b2e70277acf755ff5b40f00c3
CHROG <rogowsky@chrog.net>
2024-02-09 11:03:17 +0100

    Update logon.sh

--- logon.sh
+++ logon.sh
@@ -1,11 +1,9 @@
-mluser=`id -un`
-
-mkdir -p /Users/$mluser/Volumes
-mkdir -p /Users/$mluser/Volumes/Tausch
-mkdir -p /Users/$mluser/Volumes/Programme
-mkdir -p /Users/$mluser/Volumes/Home
-
-mount_smbfs //10.1.1.2/tausch$ /Users/$mluser/Volumes/Tausch/
-mount_smbfs //10.1.1.2/pgm$ /Users/$mluser/Volumes/Programme/
-mount_smbfs //10.1.1.2/mldata/benutzer/lul/$mluser /Users/$mluser/Volumes/Home
-mount_smbfs //10.1.1.2/mldata/benutzer/sus/$mluser /Users/$mluser/Volumes/Home
+echo "mluser=`id -un`" > /Users/logon.sh
+echo "mkdir -p /Users/$mluser/Volumes" >> /Users/logon.sh
+echo "mkdir -p /Users/$mluser/Volumes/Tausch" >> /Users/logon.sh
+echo "mkdir -p /Users/$mluser/Volumes/Programme" >> /Users/logon.sh
+echo "mkdir -p /Users/$mluser/Volumes/Home" >> /Users/logon.sh
+echo "mount_smbfs //10.1.1.2/tausch$ /Users/$mluser/Volumes/Tausch/" >> /Users/logon.sh
+echo "mount_smbfs //10.1.1.2/pgm$ /Users/$mluser/Volumes/Programme/" >> /Users/logon.sh
+echo "mount_smbfs //10.1.1.2/mldata/benutzer/lul/$mluser /Users/$mluser/Volumes/Home" >> /Users/logon.sh
+echo "mount_smbfs //10.1.1.2/mldata/benutzer/sus/$mluser /Users/$mluser/Volumes/Home" >> /Users/logon.sh
